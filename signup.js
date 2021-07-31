508e802e1d73c3dd0935437a8ba47311a18c76eb
srinivasraodandanayakula <srinivasraodandanayakula@gmail.com>
2021-07-31 22:31:33 +0530

    updated

--- signup.js
+++ signup.js
@@ -1,39 +1,105 @@
-var a=document.getElementById("Firstname").value;
-var b=document.getElementById("Email").value;
-var c=document.getElementById("Workphone").value;
-var d=document.getElementById("Homephone").value;
-var e=document.getElementById("Workaddress").value;
-var f=document.getElementById("Homeaddress").value;
-var g=document.getElementById("City").value;
 
 
-function form(){
-    if(a==""){
+
+function abc(){
+   
+
+
+    var Firstname=document.getElementById("Firstname").value;
+var Email=document.getElementById("Email").value;
+var Workphone=document.getElementById("Workphone").value;
+var Homephone=document.getElementById("Homephone").value;
+var Workaddress=document.getElementById("Workaddress").value;
+var Homeaddress=document.getElementById("Homeaddress").value;
+var City=document.getElementById("City").value;
+var popup=[];
+var popup2=[];
+
+
+
+
+
+    if(Firstname==""){
             document.getElementById("Firstname").style.borderColor ="red";
+            
         }
-   if(b=="") {
+
+        else {
+            document.getElementById("Firstname").style.borderColor ="green";
+        }
+   if(Email=="") {
             document.getElementById("Email").style.borderColor="red";
         }
 
-      if(c==""){
+        else{
+
+          document.getElementById("Email").style.borderColor ="green";
+
+        }
+      if(Workphone==""){
             document.getElementById("Workphone").style.borderColor="red";
+
+        }
+        else {
+            document.getElementById("Workphone").style.borderColor ="green";
         }
         
-     if(d==""){
+        if(Workphone.length<6){
+            popup.push('phone number cant be less than digits')
+            document.getElementById("Warning").innerText=popup.join(',')
+            document.getElementById("Warning").style.color="red"
+            document.getElementById("Workphone").style.borderColor ="red";
+        }
+
+     if(Homephone==""){
         document.getElementById("Homephone").style.borderColor="red";
+
         }
         
-      if(e==""){
+        else {
+            document.getElementById("Homephone").style.borderColor ="green";
+        }
+
+
+        if(Homephone.length>13){
+            popup2.push('phone number cant be more than 13 digits')
+            document.getElementById("Alert").innerText=popup2.join(',')
+            document.getElementById("Alert").style.color="red"
+            document.getElementById("Homephone").style.borderColor ="red";
+        }
+        
+
+      if(Workaddress==""){
           document.getElementById("Workaddress").style.borderColor="red";
         }
         
-        if(f==""){
+
+        else {
+            document.getElementById("Workaddress").style.borderColor ="green";
+
+        }
+        if(Homeaddress==""){
            document.getElementById("Homeaddress").style.borderColor="red";
         }
-        
-           if(g=="") {
-            document.getElementById("City").style.borderColor="red";
+
+        else {
+            document.getElementById("Homeaddress").style.borderColor ="green";
         }
+        
+           if(City=="") {
+            document.getElementById("City").style.borderColor="red";
+           
+        }
+        else {
+            document.getElementById("City").style.borderColor="green"
+                
+        }
+
+      
 
 
     }
+
+
+
+
